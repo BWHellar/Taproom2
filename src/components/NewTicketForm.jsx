93aafef2ff95c4669728c68ad8bea807411dd27f
Student <example@example.com>
2019-07-01 13:58:15 -0700

    give some practice to placing images

---
+++ src/components/NewTicketForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import toothy from '../../assets/images/toothy.png';
+
+function NewTicketForm(){
+  return (
+    <div>
+      <form>
+        <input
+          type='text'
+          id='names'
+          placeholder='Pair Names'/>
+        <input
+          type='text'
+          id='location'
+          placeholder='Location'/>
+        <textarea
+          id='issue'
+          placeholder='Describe your issue.'/>
+        <button type='submit'>Help!</button>
+      </form>
+      <img src={toothy}/>
+    </div>
+  );
+}
+
+export default NewTicketForm;
